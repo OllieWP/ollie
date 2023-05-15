3a5ccdb59502bde85be78217a8bd8419a2a9ba77
Mike McAlister <m@mikemcalister.com>
2023-05-14 20:09:10 -0500

    Improve header font sizes

--- patterns/header-light-with-banner.php
+++ patterns/header-light-with-banner.php
@@ -7,7 +7,7 @@
  * Keywords: header, nav, links, button
  * Viewport Width: 1500
  * Block Types: core/template-part/header
- * Post Types: 
+ * Post Types: wp_template
  * Inserter: true
  */
 
@@ -28,8 +28,8 @@
 
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large","right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"},"margin":{"top":"0px"}},"elements":{"link":{"color":{"text":"var:preset|color|main"}}}},"backgroundColor":"tertiary","layout":{"inherit":true,"type":"constrained"}} -->
 <div class="wp-block-group alignfull has-tertiary-background-color has-background has-link-color" style="margin-top:0px;padding-top:var(--wp--preset--spacing--large);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignwide"><!-- wp:site-title /-->
+<div class="wp-block-group alignwide"><!-- wp:site-title {"fontSize":"base"} /-->
 
-<!-- wp:navigation {"ref":535,"textColor":"main","overlayBackgroundColor":"base","overlayTextColor":"main","layout":{"type":"flex","orientation":"horizontal"},"style":{"spacing":{"blockGap":"20px"}}} /--></div>
+<!-- wp:navigation {"ref":535,"textColor":"main","overlayBackgroundColor":"base","overlayTextColor":"main","layout":{"type":"flex","orientation":"horizontal"},"style":{"spacing":{"blockGap":"20px"}},"fontSize":"small"} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
