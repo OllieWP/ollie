f431ad2b7adeec085309b61388012642fde36acb
Mike McAlister <m@mikemcalister.com>
2023-08-21 23:22:09 -0500

    Remove site logo conditional
    Remove site logo conditional in header patterns and revert to site title by default

--- patterns/header-dark.php
+++ patterns/header-dark.php
@@ -10,13 +10,10 @@
  * Post Types: wp_template
  * Inserter: true
  */
-
-namespace Ollie;
-
 ?>
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large","right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"},"margin":{"top":"0px"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"main","textColor":"base","layout":{"inherit":true,"type":"constrained"}} -->
 <div class="wp-block-group alignfull has-base-color has-main-background-color has-text-color has-background has-link-color" style="margin-top:0px;padding-top:var(--wp--preset--spacing--large);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignwide"><?php echo wp_kses_post( site_logo() ); ?>
+<div class="wp-block-group alignwide"><!-- wp:site-title {"fontSize":"base"} /-->
 
 <!-- wp:navigation {"ref":541,"textColor":"base","overlayBackgroundColor":"main","overlayTextColor":"base","layout":{"type":"flex","orientation":"horizontal"},"style":{"spacing":{"blockGap":"20px"}},"fontSize":"small"} /--></div>
 <!-- /wp:group --></div>
