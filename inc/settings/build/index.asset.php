7f16ff6d893374432d396a286413cf46fae56e9d
John Parris <public@johnparris.com>
2023-09-13 21:11:40 -0400

    chore: build settings assets

--- inc/settings/build/index.asset.php
+++ inc/settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '4d08210922543c637560');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'adffce5e9514cebdef38');
