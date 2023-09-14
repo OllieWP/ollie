fce0bbe29c939cb6b20d51e3e313017dac09748b
John Parris <public@johnparris.com>
2023-09-13 20:32:19 -0400

    refactor: only render OllieModal or Settings if on appropriate screen

--- inc/settings/src/index.js
+++ inc/settings/src/index.js
@@ -3,10 +3,15 @@ import { createRoot } from '@wordpress/element';
 import Settings from './settings/Settings';
 import OllieModal from './modal/OllieModal';
 
-if (options.screen === 'modal') {
-    let modal = createRoot(document.getElementById('ollie-modal'));
-    modal.render(<OllieModal/>);
-} else if (options.screen === 'settings') {
-    let settings = createRoot(document.getElementById('ollie-onboarding'));
-    settings.render(<Settings/>);
+switch (ollie_options.screen) {
+    case 'modal':
+        let modal = createRoot(document.getElementById('ollie-modal'));
+        modal.render(<OllieModal/>);
+        break;
+    case 'settings':
+        let settings = createRoot(document.getElementById('ollie-onboarding'));
+        settings.render(<Settings/>);
+        break;
+    default:
+        break;
 }
