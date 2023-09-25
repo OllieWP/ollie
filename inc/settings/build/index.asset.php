645423e0c54e5d2ce01e69951af4261faecf40ce
Mike McAlister <m@mikemcalister.com>
2023-09-25 14:34:09 -0500

    Build

--- inc/settings/build/index.asset.php
+++ inc/settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '4b7d13ee4be951887163');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '69d4e801d03b6a76923e');
