867ef5d438a619d68ab57140a673de41de345d8a
Patrick Posner <hello@patrickposner.dev>
2023-10-02 10:05:34 +0200

    removed settings

--- assets/js/notice.js
+++
@@ -1,7 +0,0 @@
-(function($) {
-    $(document).on('click', '#ollie-activation-notice .notice-dismiss', function() {
-        $.post(ajaxurl, {
-            action: 'dismiss_theme_activation_notice'
-        });
-    });
-})(jQuery);
