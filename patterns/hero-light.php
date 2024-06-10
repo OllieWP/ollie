a6e00d05024ebdf8c3efd9f351f57dbf0f7c70cf
Mike McAlister <m@mikemcalister.com>
2024-06-10 08:58:16 -0500

    Adjust font size

--- patterns/hero-light.php
+++ patterns/hero-light.php
@@ -2,16 +2,16 @@
 /**
  * Title: Hero Light
  * Slug: hero-light
- * Description:
+ * Description: 
  * Categories: ollie/call-to-action, ollie/hero
  * Keywords: cta, call to action, buttons, homepage, header, hero, heading, screenshot
  * Viewport Width: 1500
- * Block Types:
- * Post Types:
+ * Block Types: 
+ * Post Types: 
  * Inserter: true
  */
 ?>
-<!-- wp:cover {"dimRatio":90,"overlayColor":"base","isDark":false,"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"className":"is-style-blur-image-less"} -->
+<!-- wp:cover {"dimRatio":90,"overlayColor":"base","isUserOverlayColor":true,"isDark":false,"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"className":"is-style-blur-image-less"} -->
 <div class="wp-block-cover alignfull is-light is-style-blur-image-less" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><span aria-hidden="true" class="wp-block-cover__background has-base-background-color has-background-dim-90 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0px","bottom":"0px"},"padding":{"top":"var:preset|spacing|xx-large","bottom":"0px","right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"},"blockGap":"0"}},"textColor":"main","layout":{"inherit":true,"type":"constrained"}} -->
 <div class="wp-block-group alignfull has-main-color has-text-color" style="margin-top:0px;margin-bottom:0px;padding-top:var(--wp--preset--spacing--xx-large);padding-right:var(--wp--preset--spacing--medium);padding-bottom:0px;padding-left:var(--wp--preset--spacing--medium)"><!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|xx-large"}}}} -->
 <div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--xx-large)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"constrained"}} -->
@@ -19,8 +19,8 @@
 <p class="has-text-align-center has-primary-color has-text-color has-small-font-size" style="font-style:normal;font-weight:500">WordPress Reimagined</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:heading {"textAlign":"center","level":1,"className":"has-secondary-font-family","fontSize":"xxx-large"} -->
-<h1 class="wp-block-heading has-text-align-center has-secondary-font-family has-xxx-large-font-size">Build your site with clicks, not code.</h1>
+<!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"fontSize":"4rem"}},"className":"has-secondary-font-family"} -->
+<h1 class="wp-block-heading has-text-align-center has-secondary-font-family" style="font-size:4rem">Build your site with clicks, not code.</h1>
 <!-- /wp:heading --></div>
 <!-- /wp:group -->
 
@@ -39,8 +39,8 @@
 <!-- /wp:buttons --></div>
 <!-- /wp:group -->
 
-<!-- wp:image {"align":"wide","id":73,"sizeSlug":"full","linkDestination":"none","style":{"color":{"duotone":["#462CFF","#ECE8FF"]}},"className":"is-style-default"} -->
-<figure class="wp-block-image alignwide size-full is-style-default"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/patterns/images/rocket.webp" alt="" class="wp-image-73"/></figure>
+<!-- wp:image {"id":73,"sizeSlug":"full","linkDestination":"none","align":"wide","style":{"color":{"duotone":["#462CFF","#ECE8FF"]}},"className":"is-style-default"} -->
+<figure class="wp-block-image alignwide size-full is-style-default"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/patterns/images/rocket.webp" alt="" class="wp-image-73"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:group --></div></div>
 <!-- /wp:cover -->
